e4aae8368f8729e098d40f77ec4a33dd15269007
Maxim Abylkassov <maxim.abylkassov@gmail.com>
2024-09-29 13:02:01 +0000

    import os

--- app/config.py
+++ app/config.py
@@ -1,4 +1,10 @@
+import os
 class Config(object):
+    USER = os.environ.get('POSTGRES_USER')
+    PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    HOST = os.environ.get('POSTGRES_HOST')
+    PORT = os.environ.get('POSTGRES_PORT')
+    DB = os.environ.get('POSTGRES_DB')
     'SQLALCHEMY_DATABASE_URI' = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"    
     'SECRET_KEY' = 'KWBDoypJ}1WV6AkKbe]LLqz+YgGnq?UND?qE8eYtggNq>y4^Xdo_qcJC2CGxoBUMny1?oRcgHT1Md)vV>FqWo7QnC=8BjxT7g'
     'SQLALCHEMY_TRACK_MODIFICATIONS' = True 
