880f8d554c02c8dad046dde7fdc0de1cf1913c06
Maxim Abylkassov <maxim.abylkassov@gmail.com>
2024-09-29 12:39:51 +0000

    psycopg2==2.9.9

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from .extentions import db
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"    
+    app.config['SECRET_KEY'] = 'KWBDoypJ}1WV6AkKbe]LLqz+YgGnq?UND?qE8eYtggNq>y4^Xdo_qcJC2CGxoBUMny1?oRcgHT1Md)vV>FqWo7QnC=8BjxT7g'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
    # app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///project.db'
     db.init_app(app)
     
