85bab524f96b5a1370f3ad663705a86ad3d9ffa6
Maxim Abylkassov <maxim.abylkassov@gmail.com>
2024-09-29 11:32:24 +0000

    db.init_app(app)

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from .extentions import db
 
 def create_app():
     app = Flask(__name__)
+    db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
+        
     return app
