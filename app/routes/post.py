51ef265e67311281f889a3fae3f7a30e76531462
Maxim Abylkassov <maxim.abylkassov@gmail.com>
2024-10-05 06:21:43 +0000

    main were deleted, all.html template were created

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,12 +1,34 @@
-from flask import Blueprint, render_template
+from crypt import methods
+
+from flask import Blueprint, render_template, request, redirect
 from ..extensions import db
 from app.models.post import Post
 
-post = Blueprint('subject', __name__)
+post = Blueprint('post', __name__)
 
-@post.route('/post/create', method=['POST'])
+@post.route('/')
+def index():
+    return render_template('post/all.html')
+
+@post.route('/post/create', methods=['GET', 'POST'])
 def create():
-    return render_template('post/create.html')
+    if request.method == 'POST':
+        teacher = request.form['teacher']
+        subject = request.form.get('subject')
+        student = request.form.get('student')
+        date = request.form.get('date')
+
+        new_post = Post(teacher=teacher, subject=subject, student=student, date=date)
+        try:
+            db.session.add(new_post)
+            db.session.commit()
+            print('post created')
+
+            return redirect('/')
+        except Exception as e:
+            print(str(e))
+    else:
+        return render_template('post/create.html')
 
 
 
