53303f5d7847abd60cb39ed7e5d300f1d31ee8ca
Maxim Abylkassov <m.abylkassov@gmail.com>
2024-10-07 14:04:41 +0500

    user  status = db.Column(db.String(50))     login = db.Column(db.String(50))     password = db.Column(db.String(200))     date = db.Column(db.DateTime, default=datetime.utcnow)

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,10 @@
 from ..extensions import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(50), default="user")
+    status = db.Column(db.String(50))
+    login = db.Column(db.String(50))
+    password = db.Column(db.String(200))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
