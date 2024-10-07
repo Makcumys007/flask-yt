9dd7490caabdd7d4160d5a4676932a37209a5628
Maxim Abylkassov <m.abylkassov@gmail.com>
2024-10-07 15:39:27 +0500

    def register were created

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,12 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect
 from ..extensions import db
 from app.models.user import User
-
+from ..forms import RegistrationForm
 user = Blueprint('user', __name__)
 
-@user.route('/user/<name>')
-def creqte_user(name):
-    user = User(name=name)
-    db.session.add(user)
-    db.session.commit()
-    return 'User created successfully!'
+
+@user.route('/user/register')
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        print(form.name.data)
+        print(form.password.data)
+        print(form.avatar.data)
+        return redirect('/')
+    else:
+        print('Error of registration!')
+    return render_template('user/register.html', form=form)
