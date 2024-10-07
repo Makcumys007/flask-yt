c15b071f83efce48b682a9dfeed3715194769e78
Maxim Abylkassov <m.abylkassov@gmail.com>
2024-10-07 15:22:07 +0500

    validators were added to registreation forms

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms import StringField, PasswordField, SubmitField, FileField
+from wtforms.validators import Length, DataRequired, FileAllowed, EqualTo
 class RegistrationForm(FlaskForm):
-    name = StringField('Name')
-    login = StringField('Login')
-    password = PasswordField('Password')
-    confirm_password = PasswordField('Confirm Password')
+    avatar = FileField('Avatar' , validators=[FileAllowed(['jpg', 'png'])])
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
+    login = StringField('Login', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Submit')
