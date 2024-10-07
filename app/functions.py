1292944b04cc66f72190cb71b9f930ac7fda94ff
Maxim Abylkassov <m.abylkassov@gmail.com>
2024-10-07 16:15:22 +0500

    def save_picture created

---
+++ app/functions.py
@@ -0,0 +1,7 @@
+import secrets
+import os.path
+def save_picture(picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = ''
