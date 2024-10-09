8879bace35769120a7caef02f70eb0ab88cd36d6
Maxim Abylkassov <m.abylkassov@gmail.com>
2024-10-09 14:49:51 +0500

    validate_login

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FileField
+from wtforms import StringField, PasswordField, SubmitField, FileField, ValidationError
 from wtforms.validators import Length, DataRequired, EqualTo
 from flask_wtf.file import FileAllowed
+
+from app.models.user import User
+
+
 class RegistrationForm(FlaskForm):
     avatar = FileField('Avatar' , validators=[FileAllowed(['jpg', 'png'])])
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,3 +13,11 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Submit')
+
+    def validate_login(self, login):
+        user = User.query.filter_by(login=login.data).first()
+        if user:
+            raise ValidationError('This login is exist. You should use another login!')
+        
+
+        
